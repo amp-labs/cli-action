8eaa4266f94a6f96f157cc441d62bceec3cafddd
Dipu <dipuchaurasiya91@gmail.com>
2024-12-31 22:12:07 +0545

    refactor: Improve descriptions and remove environment input from action.yaml and entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,6 @@ set -e
 API_key=$1
 PROJECT_ID=$2
 DIRECTORY_PATH=$3
-ENVIRONMENT=$4
 
 
 if [ -z "$API_key" ]; then
@@ -18,17 +17,11 @@ if [ -z "$PROJECT_ID" ]; then
     exit 1
 fi
 
-if [ -z "$ENVIRONMENT"]; then
-    echo "Error: Enviroment is missing"
-    exit 1
-fi
-
-
-if !git diff --quiet HEAD^ -- "$DIRECTORY_PATH"; then
-echo "Changes detected. Deploying to $ENVIRONMENT"
+if ! git diff --quiet HEAD^ -- "$DIRECTORY_PATH"; then
+echo "Changes detected. Deploying amp file to Ampersand. "
 amp deploy "$DIRECTORY_PATH" --key "$API_key" --project "$PROJECT_ID"
-echo "::set-output name=deployment_status::success"
+echo "deployment_status=skipped" >> $GITHUB_ENV
 else
 echo "No Changes detected in $DIRECTORY_PATH. Skipping deployment"
-echo "::set-output name=deployment_status::skipped"
+echo "deployment_status=success" >> $GITHUB_ENV
 fi
